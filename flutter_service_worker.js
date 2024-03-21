60871280fcafb5a0fe9ceaad9e37cbde4b21a0ca
iComRoma <icom.ordini@gmail.com>
2024-03-21 19:17:15 +0100

    Add Icon for Edit Categories

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4d328ba5478f741cae2211ca35f673f6",
-"/": "4d328ba5478f741cae2211ca35f673f6",
-"main.dart.js": "2c40a552aea2ad3a2942ebdccd57859f",
+"index.html": "7a85ace17057c5dd30534db1495533b7",
+"/": "7a85ace17057c5dd30534db1495533b7",
+"main.dart.js": "38fb4d3e97a8e7cb1802647afb61601d",
 "manifest.json": "260e228b8b11ce544d876d55f95d7d0d",
 "version.json": "b7768ceafd67184f8c943dd0188a5639"};
 // The application shell files that are downloaded before a service worker can
