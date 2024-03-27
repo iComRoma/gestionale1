904a16e17e5ef4f1e3b2852d9c4945e5cd63d05e
iComRoma <icom.ordini@gmail.com>
2024-03-27 11:19:08 +0100

    Updating both the Category and Product images

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "39e41f25afce226ecce3a8d7055f71c1",
-"/": "39e41f25afce226ecce3a8d7055f71c1",
-"main.dart.js": "c6906967980e7e513b388bcd6b3e58d0",
+"index.html": "02bd533b6bcd0afa4244e09a913bfe00",
+"/": "02bd533b6bcd0afa4244e09a913bfe00",
+"main.dart.js": "6a2ddaaa93bd3f371cf7d72fb63e9fae",
 "manifest.json": "260e228b8b11ce544d876d55f95d7d0d",
 "version.json": "b7768ceafd67184f8c943dd0188a5639"};
 // The application shell files that are downloaded before a service worker can
