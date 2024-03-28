5bb53837863185cba0147c9545e446d0693b35fe
iComRoma <icom.ordini@gmail.com>
2024-03-28 17:41:11 +0100

    Add search to Modal Parent Categ Select

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "17b5846e446b95ffcd9a16be25ced281",
-"/": "17b5846e446b95ffcd9a16be25ced281",
-"main.dart.js": "6eaa01071ee60081a5465040668ae222",
+"index.html": "f92474e28ddcd6b54fa5ac96969c1d1a",
+"/": "f92474e28ddcd6b54fa5ac96969c1d1a",
+"main.dart.js": "174d80a4a29d26278bc076953c7f2477",
 "manifest.json": "260e228b8b11ce544d876d55f95d7d0d",
 "version.json": "b7768ceafd67184f8c943dd0188a5639"};
 // The application shell files that are downloaded before a service worker can
